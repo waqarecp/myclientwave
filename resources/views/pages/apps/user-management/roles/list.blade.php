b017f09be9f40364e94a4dfd195e59a567c09742
Waqar Ahmad <waqarecp1992@gmail.com>
2024-09-19 01:44:54 +0500

    roles refactored

--- resources/views/pages/apps/user-management/roles/list.blade.php
+++ resources/views/pages/apps/user-management/roles/list.blade.php
@@ -8,6 +8,11 @@
         {{ Breadcrumbs::render('user-management.roles.index') }}
     @endsection
 
+    @if (session('error'))
+        <div class="alert alert-danger col-md-6 offset-md-3">
+            {{ session('error') }}
+        </div>
+    @endif
     <!--begin::Content container-->
     <div id="kt_app_content_container" class="app-container container-xxl">
         <livewire:permission.role-list></livewire:permission.role-list>
